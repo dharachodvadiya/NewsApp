f881c70454057e438d0adfe010431dae093f4e53
Dhara Boghra <81017196+dharachodvadiya@users.noreply.github.com>
2024-07-11 13:31:43 +0400

    Update Constant.kt

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constant.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constant.kt
@@ -1,7 +1,7 @@
 package com.androiddevs.mvvmnewsapp.util
 
 object Constant {
-    const val API_KEY = "cbc379902b704600b9785b5f920d75dc"
+    const val API_KEY = "your_news_api_key"
     const val BASE_URL = "https://newsapi.org"
     const val SEARCH_NEWS_TIME_DELAY = 500L
     const val QUERY_PAGE_SIZE = 20
