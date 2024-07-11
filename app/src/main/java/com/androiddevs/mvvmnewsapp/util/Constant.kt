2baf789aed9404edd26d862094625a199783523d
Dhara <dhara00719@gmail.com>
2024-07-11 14:05:42 +0400

    fix issue

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constant.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constant.kt
@@ -1,7 +1,9 @@
 package com.androiddevs.mvvmnewsapp.util
 
+import com.androiddevs.mvvmnewsapp.BuildConfig
+
 object Constant {
-    const val API_KEY = "your_news_api_key"
+    val API_KEY = BuildConfig.NEWS_API_KEY
     const val BASE_URL = "https://newsapi.org"
     const val SEARCH_NEWS_TIME_DELAY = 500L
     const val QUERY_PAGE_SIZE = 20
